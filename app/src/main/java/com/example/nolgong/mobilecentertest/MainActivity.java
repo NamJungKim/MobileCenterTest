35977262cad1202d0178ff5e3ddf9bc57e57f1fe
NamJungKim <pom2398@gmail.com>
2017-07-26 17:35:54 +0900

    Add Mobile Center SDK

--- app/src/main/java/com/example/nolgong/mobilecentertest/MainActivity.java
+++ app/src/main/java/com/example/nolgong/mobilecentertest/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileCenter.start(getApplication(), "c72d1e69-28d9-4e98-ac29-b2c4fbbdcfe7",
+                Analytics.class, Crashes.class);
+
         first = (Button)findViewById(R.id.first);
         second = (Button)findViewById(R.id.second);
         third = (Button)findViewById(R.id.third);
