693d4514859cac34ebf26c600b6559151f00597b
NamJungKim <pom2398@gmail.com>
2017-07-28 09:39:33 +0900

    listener add

--- app/src/main/java/com/example/nolgong/mobilecentertest/MainActivity.java
+++ app/src/main/java/com/example/nolgong/mobilecentertest/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.nolgong.mobilecentertest;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -13,6 +16,10 @@ import com.microsoft.azure.mobile.MobileCenter;
 import com.microsoft.azure.mobile.analytics.Analytics;
 import com.microsoft.azure.mobile.crashes.Crashes;
 import com.microsoft.azure.mobile.push.Push;
+import com.microsoft.azure.mobile.push.PushListener;
+import com.microsoft.azure.mobile.push.PushNotification;
+
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,16 +33,46 @@ public class MainActivity extends AppCompatActivity {
 
         MobileCenter.start(getApplication(), "c72d1e69-28d9-4e98-ac29-b2c4fbbdcfe7",
                 Analytics.class, Crashes.class);
-        MobileCenter.start(getApplication(), "c72d1e69-28d9-4e98-ac29-b2c4fbbdcfe7", Push.class);
 
         Push.enableFirebaseAnalytics(getApplication());
         MobileCenter.start(getApplication(), "c72d1e69-28d9-4e98-ac29-b2c4fbbdcfe7", Push.class);
+        Push.setListener(new PushListener() {
+            @Override
+            public void onPushNotificationReceived(Activity activity, PushNotification pushNotification) {
+                Log.d("MobileCenter Push ","pushNotification.getMessage()");
+                /* The following notification properties are available. */
+                String title = pushNotification.getTitle();
+                String message = pushNotification.getMessage();
+                Map<String, String> customData = pushNotification.getCustomData();
+
+        /*
+         * Message and title cannot be read from a background notification object.
+         * Message being a mandatory field, you can use that to check foreground vs background.
+         */
+                if (message != null) {
+
+            /* Display an alert for foreground push. */
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
+                    if (title != null) {
+                        dialog.setTitle(title);
+                    }
+                    dialog.setMessage(message);
+                    if (!customData.isEmpty()) {
+                        dialog.setMessage(message + "\n" + customData);
+                    }
+                    dialog.setPositiveButton(android.R.string.ok, null);
+                    dialog.show();
+                } else {
+
+            /* Display a toast when a background push is clicked. */
+                    Toast.makeText(activity, String.format(activity.getString(R.string.push_toast), customData), Toast.LENGTH_LONG).show(); // For example R.string.push_toast would be "Push clicked with data=%1s"
+                }
+            }
+        });
 
         Log.d("token : ", FirebaseInstanceId.getInstance().getToken());
         FirebaseMessaging.getInstance().subscribeToTopic("notice");
 
-
-        Push.setListener(new MyPushListener());
         MobileCenter.start();
 
         first = (Button)findViewById(R.id.first);
